4ff24b027d7687ed3392728f1149b97ae6cf3fa5
huoju <virushuo@gmail.com>
2019-06-29 01:44:38 -0400

    fetch tweets forward/backward

--- src/actionne_twitter/core.clj
+++ src/actionne_twitter/core.clj
@@ -1,4 +1,4 @@
-(ns actionne-twitter.core
+(ns actionne_twitter.core
     (:require
             [config.core :refer [env]]
             [twttr.api :as api]
@@ -8,15 +8,19 @@
     (:import (java.io BufferedWriter FileWriter))
 )
 
-(def homedir
-    (or (env :sg-home) "/tmp/sghome")
-)  
+(defn expand-home [s]
+  (if (.startsWith s "~")
+    (clojure.string/replace-first s "~" (System/getProperty "user.home"))
+    s))
+
+(def homedir 
+  (expand-home (or (env :actionne-home) "~/actionne")))
 
 (def today
     (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") (new java.util.Date))
 )
 
-(let [wtr (agent (BufferedWriter. (FileWriter. (str homedir "/backup/" "actionne-twitter-" today ".log") true)))]
+(let [wtr (agent (BufferedWriter. (FileWriter. (str homedir "/data/" "actionne_twitter-" today ".log") true)))]
     (defn save [msg]
       (letfn [(write [out msg]
               (.write out msg) 
@@ -26,8 +30,53 @@
       (defn close []
             (send wtr #(.close %))))
 
+(defn loadsession []
+    (let [filename (str homedir "/data/" "actionne_twitter-session.clj")]
+        (try
+            (read-string (slurp filename))
+            (catch Exception e {}))
+    )
+)
+
+(defn updatesession [session]
+    (let [filename (str homedir "/data/" "actionne_twitter-session.clj")]
+        (with-open [w (clojure.java.io/writer filename)]
+            (binding [*out* w]
+        (pr session)))
+    )
+)
+
+(defn initsession []
+    (updatesession {:stage "init"})
+)
+
+(defn paramsname [stage]
+    (if (= "init" stage)
+        "min"
+        "max"
+    )
+)
+
+(defn timelineparams [session]
+
+    (if (= "init" (:stage session))
+        (if (not= (:pending session) nil)  ;init stage, with id
+            {:max_id (:last_id (:confirmed session))}
+            {} 
+        )
+        (if (not= (:pending session) nil)  ;normal stage, with id
+            {:since_id (:last_id (:confirmed session))}
+            {} 
+        )
+    ))
+
 
 (defn backup [original]
+    (println (str "save.." (:id original)))
+    ;(if (contains? (:entities original) :media)
+    ;    (do (println "===media")
+    ;    (prn (:media (:entities original))))
+    ;)
     (save (generate-string original))
     (save "\n")
 )
@@ -49,27 +98,72 @@
 )
 
 
-(defn fetchtweets [env]
-    (let [creds (map->UserCredentials env )]
-        (let [tweets (api/statuses-user-timeline creds :params {:screen_name "virushuo"})]
-            (map (fn [tweet] 
-                    (let [  favorite_count (:favorite_count tweet) 
-                            id (:id_str tweet) 
-                            retweet_count (:retweet_count tweet)  
-                            text (:text tweet)  
-                            in_reply_to_status_id (:in_reply_to_status_id tweet)
-                            in_reply_to_user_id (:in_reply_to_user_id tweet)
+(defn lastid[session]
+    (if (not= (:confirmed session) nil) 
+        (:confirmed session)
+        {}
+    )
+)
 
-                         ]
+(defn fetchtweets [env session]
+    ;(prn env)
+    (let [creds (map->UserCredentials env) 
+          screen_name (:screen_name env)]
+        ;(if (= (:confirmed session) nil) 
+        ;    ;{:screen_name (:screen_name env)} 
+        ;    ;{:max_id (:last_id session) :screen_name (:screen_name env)}
+        ;    (:confirmed (loadsession))
+        ;)
+        (prn (lastid session))
+        (prn (timelineparams (loadsession)))
 
-                       {:id id 
-                        :object {:favorite_count favorite_count :retweet_count retweet_count :text text
-                                 :category (if (or (not (nil? in_reply_to_status_id))  (not (nil? in_reply_to_user_id)))
-                                     "tweet"
-                                     "reply")} 
-                        :original tweet})) 
-            tweets)
+        (let [formattedtweets (let [tweets (api/statuses-user-timeline creds :params (merge {:screen_name screen_name} (timelineparams (loadsession))))]
+                (map (fn [tweet] 
+                        (let [  favorite_count (:favorite_count tweet) 
+                                id (:id_str tweet) 
+                                retweet_count (:retweet_count tweet)  
+                                text (:text tweet)  
+                                in_reply_to_status_id (:in_reply_to_status_id tweet)
+                                in_reply_to_user_id (:in_reply_to_user_id tweet)
+
+                             ]
+
+                           {:id id 
+                            :object {:favorite_count favorite_count :retweet_count retweet_count :text text
+                                     :category (if (or (not (nil? in_reply_to_status_id))  (not (nil? in_reply_to_user_id)))
+                                        "reply"
+                                        "tweet")} 
+                            :original tweet})) 
+                tweets)
+            )]
+            (if (> 0 (count formattedtweets))
+                (let [maxid (apply (resolve (symbol (paramsname (:stage (loadsession))))) (map read-string (map :id formattedtweets)))]
+                    (updatesession 
+                        (assoc session :pending {:last_id maxid})
+                    )
+                )
+            )
+            formattedtweets
         )
     )
+)
+
+
+(defn confirmtask[]
+    (let [session (loadsession)]
+        (if (not= (:pending session) nil) 
+           (updatesession (assoc session :confirmed (:pending session)))
+        )
+    )
+)
+
+
+(defn run [env]
+    (println "run")
+    (let [session (loadsession)]
+       (fetchtweets env session)
+       ;(if (= (:last_id (loadsession)) nil) (println "nil") (println "id"))
+    )
+
 )
 
--- test/actionne_twitter/core_test.clj
+++ test/actionne_twitter/core_test.clj
@@ -1,6 +1,6 @@
-(ns actionne-twitter.core-test
+(ns actionne_twitter.core-test
   (:require [clojure.test :refer :all]
-            [actionne-twitter.core :refer :all]))
+            [actionne_twitter.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
