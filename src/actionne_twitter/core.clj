49a46a995d56f43c2a9028cdf207cdeaccc3d467
huoju <virushuo@gmail.com>
2019-08-05 01:55:22 -0400

    call api to delete tweets

--- src/actionne_twitter/core.clj
+++ src/actionne_twitter/core.clj
@@ -27,7 +27,7 @@
     (java.text.SimpleDateFormat. "EEE MMM dd HH:mm:ss ZZZZZ yyyy" (Locale. "english"))
 )
 
-(let [wtr (agent (BufferedWriter. (FileWriter. (str homedir "/data/" "actionne_twitter-" today ".log") true)))]
+(let [wtr (agent (BufferedWriter. (FileWriter. (str homedir "/data/" "actionne_twitter-" today ".backup.log") true)))]
     (defn save [msg]
       (letfn [(write [out msg]
               (.write out msg) 
@@ -36,6 +36,7 @@
           (send wtr write msg)))
       (defn close []
             (send wtr #(.close %))))
+
 
 (defn loadsession [account]
     (let [filename (str homedir "/data/" account "-actionne_twitter-session.clj")]
@@ -120,19 +121,40 @@
         )
     )
 )
-
-(defn delete [id original]
-    (log/info (str "delete: " (:id original)))
+(defn deletetweet [id env]
+    (try
+        (let [creds (map->UserCredentials env) screen_name (:screen_name env)]
+            (log/info (str "delete tweet: " id ))
+            (api/statuses-destroy-id creds :params {:id id})
+            nil
+        )
+    (catch Exception e 
+        (log/error (str "delete tweet: " id " " (:cause (Throwable->map e))))
+        nil
+    ))
 )
 
-;(defn done []
-;    (close)
-;)
+(defn delete [config id original]
+    (log/info (str "delete: " (:id original)))
+    (if (or (= false (:dryloop (:actionne_twitter config))) (nil? (:dryloop (:actionne_twitter config))))
+        (if (= true (:backup (:actionne_twitter config)))
+            (doall (backup original)
+                (deletetweet id (:actionne_twitter config))
+            )
+        )
+        (log/info (str "dryloop action delete, skip " id))
+    )
+)
 
 
-(defn notify [id original]
+(defn notify [config id original]
     (log/info (str "notifiy: " id))
-    (backup original)
+    (if (or (= false (:dryloop (:actionne_twitter config))) (nil? (:dryloop (:actionne_twitter config))))
+        (if (= true (:backup (:actionne_twitter config)))
+            (backup original)
+        )
+        (log/info (str "dryloop action notify, skip " id))
+    )
 )
 
 (defn lastid[session]
@@ -142,19 +164,13 @@
     )
 )
 
+
+
+
 (defn fetchtweets [env session]
-    ;(prn env)
     (let [creds (map->UserCredentials env) screen_name (:screen_name env)]
-        ;(if (= (:confirmed session) nil) 
-        ;    ;{:screen_name (:screen_name env)} 
-        ;    ;{:max_id (:last_id session) :screen_name (:screen_name env)}
-        ;    (:confirmed (loadsession))
-        ;)
         (log/info (str "fetch: " (merge {:screen_name screen_name} (timelineparams (loadsession screen_name)))))
         (let [formattedtweets (let [tweets (api/statuses-user-timeline creds :params (merge {:screen_name screen_name} (timelineparams (loadsession screen_name))))]
-
-
-
                 (map (fn [tweet] 
                         (let [  favorite_count (:favorite_count tweet) 
                                 id (:id_str tweet) 
@@ -162,33 +178,44 @@
                                 text (:text tweet)
                                 created_at (.getTime  (.parse df (:created_at tweet)))
                                 media_urls (mediaurls tweet)
+                                retweeted_status (:retweeted_status tweet)
                                 in_reply_to_status_id (:in_reply_to_status_id tweet)
                                 in_reply_to_user_id (:in_reply_to_user_id tweet)
                              ]
 
                            {:id id 
-                            :object {:favorite_count favorite_count :retweet_count retweet_count :text text :created_at created_at 
+                            :object {:id id :favorite_count favorite_count :retweet_count retweet_count :text text :created_at created_at 
                                      :media_urls media_urls
-                                     :category (if (or (not (nil? in_reply_to_status_id))  (not (nil? in_reply_to_user_id)))
-                                        "reply"
-                                        "tweet")} 
+                                     :category (cond 
+                                                    (or (not (nil? in_reply_to_status_id))  (not (nil? in_reply_to_user_id))) "reply" 
+                                                    (not (nil? retweeted_status)) "retweet" 
+                                                    :else "tweet")
+                                    } 
                             :original tweet})) 
                 tweets)
             )]
             (log/info (str "tweets count:" (count formattedtweets)))
-            (if (> 0 (count formattedtweets))
+            (if (> (count formattedtweets) 0)
                 (let [maxid (apply (resolve (symbol (paramsname (:stage (loadsession screen_name))))) (map read-string (map :id formattedtweets)))]
-                    (updatesession screen_name (assoc session :pending {:last_id maxid})
-                    )
-                )
+                    (updatesession screen_name (assoc session :pending {:last_id maxid})))
+                (if (= "init" (:stage (loadsession screen_name)))
+                    (doall (updatesession screen_name (assoc session :stage "normal"))
+                    (log/info (str "Init stage finished. Turn to the normal stage."))))
             )
             formattedtweets
         )
     )
 )
+(defn before [env]
+    (log/info (str "call actionne_twitter/core.before for account: " (:screen_name env)))
+    (if (= {} (loadsession (:screen_name env)))
+        (initsession (:screen_name env))    
+    )
+)
 
 
-(defn confirmtask[env]
+(defn success [env]
+    (log/info (str "call actionne_twitter/core.success for account: " (:screen_name env)))
     (let [session (loadsession (:screen_name env))]
         (if (not= (:pending session) nil) 
            (updatesession (:screen_name env) (assoc session :confirmed (:pending session)))
@@ -196,19 +223,15 @@
     )
 )
 
+(defn failure [env]
+    (log/info (str "call actionne_twitter/core.failure for account: " (:screen_name env)))
+)
 
 
 (defn run [env]
-
-    (log/info (str "plugin actionne_twitter running for account: " (:screen_name env)))
+    (log/info (str "call actionne_twitter/core.run for account: " (:screen_name env)))
     (let [session (loadsession (:screen_name env))]
        (fetchtweets env session)
-       ;(let [tweets (fetchtweets env session)]
-       ;     (map (fn [tweet] 
-       ;         (backup (:original tweet))
-       ;     ) tweets)
-       ;)
     )
-
 )
 
